0b661adfa98f2864a05c7f0c52e0dc34d54ee8ae
Rwothoromo Elijah <rwothoromoelijah@gmail.com>
2019-08-01 16:10:18 +0300

    Save to Firebase:
    - Travel Deals Model and UI
    - Logic to save to Firebase

---
+++ app/src/main/java/com/example/travelmantics/activities/InsertActivity.kt
@@ -0,0 +1,70 @@
+package com.example.travelmantics.activities
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.travelmantics.R
+import com.example.travelmantics.models.TravelDeal
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_insert.*
+
+class InsertActivity : AppCompatActivity() {
+
+    // entry point for the db
+    private var mFirebaseDatabase: FirebaseDatabase? = null
+
+    // location in the db where we read/write data
+    private var mDatabaseReference: DatabaseReference? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insert)
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance()
+
+        // the path we want to reach
+        mDatabaseReference = mFirebaseDatabase!!.reference.child("traveldeals")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.save_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.save_menu -> {
+                saveDeal()
+                Toast.makeText(this, "Deal saved", Toast.LENGTH_LONG).show()
+
+                // reset contents of the edit text fields
+                clean()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun saveDeal() {
+        val title = txtTitle.text.toString()
+        val description = txtDescription.text.toString()
+        val price = txtPrice.text.toString()
+
+        val travelDeal = TravelDeal(title, description, price)
+
+        // insert object to db
+        mDatabaseReference?.push()?.setValue(travelDeal)
+    }
+
+    private fun clean() {
+        txtTitle.text = null
+        txtDescription.text = null
+        txtPrice.text = null
+
+        // give focus to the title field
+        txtTitle.requestFocus()
+    }
+}
