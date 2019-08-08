e29b3d3bf22dbece9b19b8bbb43697e4939deb2f
Rwothoromo Elijah <rwothoromoelijah@gmail.com>
2019-08-08 04:44:05 +0300

    Added code for file storage on firebase

--- app/src/main/java/com/example/travelmantics/activities/TravelDealActivity.kt
+++ app/src/main/java/com/example/travelmantics/activities/TravelDealActivity.kt
@@ -1,17 +1,27 @@
 package com.example.travelmantics.activities
 
+import android.app.Activity
 import android.content.Intent
+import android.content.res.Resources
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.travelmantics.R
+import com.example.travelmantics.constants.Constants.PICTURE_RESULT
 import com.example.travelmantics.models.TravelDeal
 import com.example.travelmantics.utils.FirebaseUtil
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import kotlinx.android.synthetic.main.activity_insert.*
+import com.google.firebase.storage.UploadTask
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_deal.*
+
 
 class TravelDealActivity : AppCompatActivity() {
 
@@ -25,7 +35,7 @@ class TravelDealActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_insert)
+        setContentView(R.layout.activity_deal)
 
         mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase
         mDatabaseReference = FirebaseUtil.mDatabaseReference
@@ -39,19 +49,73 @@ class TravelDealActivity : AppCompatActivity() {
         txtTitle.setText(travelDeal.title)
         txtDescription.setText(travelDeal.description)
         txtPrice.setText(travelDeal.price)
+
+        // show image
+        travelDeal.imageUrl?.let { showImage(it) }
+
+        btnImage.setOnClickListener {
+            intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/jpg"
+            intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true)
+            startActivityForResult(
+                Intent.createChooser(intent, "Insert Picture"),
+                PICTURE_RESULT
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICTURE_RESULT && resultCode == Activity.RESULT_OK) {
+            // locate the image file to be uploaded and put the location in a Uri object
+            val imageUri = data!!.data!!
+
+            // get the reference to the storage, where the data should be uploaded
+            val storageReference = FirebaseUtil.mStorageReference!!.child(imageUri.lastPathSegment!!)
+
+            // putFile will return an asynchronous upload task task
+            // where we can listen to the result; success or failure
+            val uploadTask = storageReference.putFile(imageUri)
+
+            val urlTask = uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                val pictureName = task.result?.storage?.path
+                travelDeal.imageName = pictureName
+
+                return@Continuation storageReference.downloadUrl
+            })
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val downloadUri = task.result
+                        val url = downloadUri.toString()
+                        travelDeal.imageUrl = url
+                        showImage(url)
+                    } else {
+                        // Handle failures
+                        // ...
+                    }
+                }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.save_menu, menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
 
         if (FirebaseUtil.isAdmin) {
             menu?.findItem(R.id.delete_menu)?.isVisible = true
             menu?.findItem(R.id.save_menu)?.isVisible = true
             enableEditTexts(true)
+            menu?.findItem(R.id.btnImage)?.isVisible = true
         } else {
             menu?.findItem(R.id.delete_menu)?.isVisible = false
             menu?.findItem(R.id.save_menu)?.isVisible = false
             enableEditTexts(false)
+            menu?.findItem(R.id.btnImage)?.isVisible = false
         }
 
         return true
@@ -88,6 +152,7 @@ class TravelDealActivity : AppCompatActivity() {
         travelDeal.title = txtTitle.text.toString()
         travelDeal.description = txtDescription.text.toString()
         travelDeal.price = txtPrice.text.toString()
+        travelDeal.imageUrl?.let { Log.d("url", it) }
 
         if (travelDeal.id == null) {
             // insert object to db
@@ -99,6 +164,11 @@ class TravelDealActivity : AppCompatActivity() {
     }
 
     private fun deleteDeal() {
+
+        // delete image from firebase storage
+        if (travelDeal.imageName != null) {
+            FirebaseUtil.mFirebaseStorage!!.reference.child(travelDeal.imageName!!).delete()
+        }
 
         // delete the db value with the matching id
         mDatabaseReference?.child(travelDeal.id.toString())?.removeValue()
@@ -122,5 +192,15 @@ class TravelDealActivity : AppCompatActivity() {
         txtTitle.isEnabled = isEnabled
         txtDescription.isEnabled = isEnabled
         txtPrice.isEnabled = isEnabled
+    }
+
+    private fun showImage(url: String) {
+        val width = Resources.getSystem().displayMetrics.widthPixels
+
+        Picasso.get()
+            .load(url)
+            .resize(width, width * 2 / 3)
+            .centerCrop()
+            .into(image)
     }
 }
---
+++ app/src/main/java/com/example/travelmantics/constants/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.travelmantics.constants
+
+object Constants {
+
+    const val RC_SIGN_IN = 123 // request code assigned for starting the new activity, it can be any number
+    const val PICTURE_RESULT = 42
+
+}
